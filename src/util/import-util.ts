5b0be5a7cf350fa0fd794446bf72cffec9100e98
x7airworker <jake@jakes-co.de>
2019-08-27 16:36:13 +0200

    Changed back to problem

--- src/util/import-util.ts
+++ src/util/import-util.ts
@@ -4,11 +4,12 @@ declare type LuaEnv = {[key: string]: any};
 
 declare const _ENV: LuaEnv;
 
-function importClass<T extends any>(name: string): () => T {
-    return () => _ENV[name] as T;
+function importClass<T extends any>(t: T&Function): () => T {
+    return () => _ENV[t] as T;
 }
 
 AddFunctionExport("importClass", importClass);
 
-const gettedPlayer = importClass("Player")();
-const instance: Player = new gettedPlayer();
+const gettedPlayer = importClass(Player)();
+const instance: Player = new gettedPlayer(20);
+print(instance.getHealth());
