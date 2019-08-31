7739840c3cffbd96ae7b6eb48b78e159d181227e
x7airworker <jake@jakes-co.de>
2019-08-31 14:59:55 +0200

    Used stdlib. Made Client and Server a namespace

--- src/client/init.ts
+++ src/client/init.ts
@@ -1,8 +1,8 @@
 /** @noSelfInFile */
 
 let lastSoundPlayed: Sound;
-let textRaceInfo: TextBox|undefined;
-let updateTimer: Timer|undefined;
+let textRaceInfo: TextBox | undefined;
+let updateTimer: Timer | undefined;
 let currentCheckpoint: number;
 let maxCheckpoints: number;
 let currentPosition: number;
@@ -17,37 +17,35 @@ function playAudioFile(file: string): void {
 Client.listenRemote("PlayAudioFile2", playAudioFile);
 
 Client.listenRemote("RaceCountdown", (time: number) => {
-    Client.printChat("Race countdown: " + time);
+	Client.printChat("Race countdown: " + time);
 	let countdownText = Client.createTextBox(new Vector2d(0.0, -200.0), '<span size="80" style="italic" color="#42f448">GO GO GO!</>', "center");
 	countdownText.setAnchors(new Vector2d(0.5, 0.5), new Vector2d(0.5, 0.5));
 	countdownText.setAlignment(new Vector2d(0.5, 0.5));
-    if (time == 3) {
-        playAudioFile("3.mp3");
+	if (time == 3) {
+		playAudioFile("3.mp3");
 		countdownText.setText('<span size="46" style="bold" color="#fc0000">3</>');
-    } else if (time == 2) {
-        playAudioFile("2.mp3");
+	} else if (time == 2) {
+		playAudioFile("2.mp3");
 		countdownText.setText('<span size="52" style="bold" color="#fc4300">2</>');
-    } else if (time == 1) {
-        playAudioFile("1.mp3");
+	} else if (time == 1) {
+		playAudioFile("1.mp3");
 		countdownText.setText('<span size="58" style="bold" color="#fcc900">1</>');
-    } else if (time == 0) {
-        playAudioFile("go.mp3");
+	} else if (time == 0) {
+		playAudioFile("go.mp3");
 		playAudioFile("MLG_Horn.mp3");
-    }
-    Client.delay(900,()=>countdownText.destroy());
+	}
+	Client.delay(900, () => countdownText.destroy());
 });
 
 Client.listenRemote("OnRaceJoin", (worldTime: number) => {
-	Client.setIgnoreMoveInput(true);
-	Client.setIgnoreLookInput(true);
+	setInput(true);
 	if (worldTime && worldTime != -1)
 		Client.getWorld().setTime(worldTime);
 });
 
 Client.listenRemote("OnRaceStart", (timeForRace: number) => {
-    Client.printChat('<span color="#88eb00" size="18">The race starts now!</>');
-	Client.setIgnoreMoveInput(false);
-	Client.setIgnoreLookInput(false);
+	Client.printChat('<span color="#88eb00" size="18">The race starts now!</>');
+	setInput(false);
 	textRaceInfo = Client.createTextBox(new Vector2d(50.0, 100.0), "-", "left");
 	textRaceInfo.setAnchors(new Vector2d(0.0, 0.5), new Vector2d(0.0, 0.5));
 	textRaceInfo.setAlignment(new Vector2d(0.0, 0.5));
@@ -57,13 +55,12 @@ Client.listenRemote("OnRaceStart", (timeForRace: number) => {
 });
 
 Client.listenRemote("OnRaceExit", () => {
-	Client.setIgnoreMoveInput(false);
-	Client.setIgnoreLookInput(false);
-	if(textRaceInfo !== undefined){
+	setInput(false);
+	if (textRaceInfo !== undefined) {
 		textRaceInfo.destroy();
 		textRaceInfo = undefined;
 	}
-	if(updateTimer !== undefined){
+	if (updateTimer !== undefined) {
 		updateTimer.destroy();
 		updateTimer = undefined;
 	}
@@ -87,9 +84,9 @@ function setRaceTextInfo(check: number, maxChecks: number, pos: number, maxRacer
 		let vehicle = player.getVehicle();
 		let speed = Math.floor(vehicle.getSpeed());
 		let gear = GetVehicleGear(vehicle.getGear());
-	    let rpm = Math.floor(vehicle.getRPM());
+		let rpm = Math.floor(vehicle.getRPM());
 		let health = Math.floor(vehicle.getHealth());
-	    let time_left = raceTime - (Client.getTimeSeconds() - raceStartTime);
+		let time_left = raceTime - (Client.getTimeSeconds() - raceStartTime);
 		textRaceInfo.setText(`
 			<span size="30" style="bold" color="#fc4300">Checkpoint: ${check}/${maxChecks}</>
 			<span size="30" style="bold" color="#fc4300">Time: ${formatTime(time_left)}</> 
@@ -109,7 +106,15 @@ function formatTime(time: number): string {
 }
 
 function pad(num: number, size: number) {
-    var s = num+"";
-    while (s.length < size) s = "0" + s;
-    return s;
+	var s = num + "";
+	while (s.length < size) s = "0" + s;
+	return s;
 }
+
+function setInput(val: boolean) {
+	Client.setIgnoreMoveInput(val);
+	Client.setIgnoreLookInput(val);
+}
+
+const d = new Dimension(20);
+d.setDimension(new Vehicle(2));
