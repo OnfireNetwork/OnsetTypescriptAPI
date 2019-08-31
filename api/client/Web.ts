6145a3aab804573a99c049680200c8f343f822d1
x7airworker <jake@jakes-co.de>
2019-08-31 17:11:45 +0200

    A tiny bit progress on Client implementation

--- api/client/Web.ts
+++ api/client/Web.ts
@@ -11,7 +11,25 @@ class Web {
     public setURL(url: string) {
         SetWebURL(this.id, url);
     }
-    public setSize(x: number, y: number) {
-        SetWebSize(this.id, x, y);
+    public setSize(size: Vector2d) {
+        SetWebSize(this.id, size.x, size.y);
+    }
+    public isVisible(): boolean {
+        return GetWebVisibility(this.id);
+    }
+    public setVisible(visiblity: boolean) {
+        SetWebVisibility(this.id, visiblity);
+    }
+    public loadWebFile(file: string) {
+        LoadWebFile(this.id, file);
+    }
+    public execJS(source: string) {
+        ExecuteWebJS(this.id, source);
+    }
+    public setLocation(position: Vector2d, z?: number): void {
+        SetWebLocation(this.id, position.x, position.y, z);
+    }
+    public setAlignment(alignment: Vector2d) {
+        SetWebAlignment(this.id, alignment.x, alignment.y);
     }
 }
