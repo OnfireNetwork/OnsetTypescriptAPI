33d6fd3260e007b2f0f9caa469bea1d82c395d08
JanHolger <jan@bebendorf.eu>
2019-09-22 18:42:37 +0200

    Fixed callEvent

--- onset/client/Client.ts
+++ onset/client/Client.ts
@@ -56,11 +56,67 @@ namespace Client {
     export function listenRemote(event: string, handler: Function): void {
         AddRemoteEvent(event, handler);
     }
-    export function call(event: string, ...args: any[]): void {
-        CallEvent(event, args);
+    export function call(event: string, ...values: any[]): void {
+        switch(values.length){
+            case 0:
+                CallEvent(event);
+                break;
+            case 1:
+                CallEvent(event, values[0]);
+                break;
+            case 2:
+                CallEvent(event, values[0], values[1]);
+                break;
+            case 3:
+                CallEvent(event, values[0], values[1], values[2]);
+                break;
+            case 4:
+                CallEvent(event, values[0], values[1], values[2], values[3]);
+                break;
+            case 5:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4]);
+                break;
+            case 6:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4], values[5]);
+                break;
+            case 7:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+                break;
+            default:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+                break;
+        }
     }
-    export function callRemote(event: string, ...args: any[]): void {
-        CallRemoteEvent(event, args);
+    export function callRemote(event: string, ...values: any[]): void {
+        switch(values.length){
+            case 0:
+                CallRemoteEvent(event);
+                break;
+            case 1:
+                CallRemoteEvent(event, values[0]);
+                break;
+            case 2:
+                CallRemoteEvent(event, values[0], values[1]);
+                break;
+            case 3:
+                CallRemoteEvent(event, values[0], values[1], values[2]);
+                break;
+            case 4:
+                CallRemoteEvent(event, values[0], values[1], values[2], values[3]);
+                break;
+            case 5:
+                CallRemoteEvent(event, values[0], values[1], values[2], values[3], values[4]);
+                break;
+            case 6:
+                CallRemoteEvent(event, values[0], values[1], values[2], values[3], values[4], values[5]);
+                break;
+            case 7:
+                CallRemoteEvent(event, values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+                break;
+            default:
+                CallRemoteEvent(event, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+                break;
+        }
     }
     export function exportFunction(name: string, exportFunction: Function): void {
         AddFunctionExport(name, exportFunction);
--- onset/server/Server.ts
+++ onset/server/Server.ts
@@ -49,11 +49,70 @@ namespace Server {
     export function listenRemote(event: string, handler: Function): void {
         AddRemoteEvent(event, handler);
     }
-    export function call(event: string, ...args: any[]): void {
-        CallEvent(event, args);
+    export function call(event: string, ...values: any[]): void {
+        switch(values.length){
+            case 0:
+                CallEvent(event);
+                break;
+            case 1:
+                CallEvent(event, values[0]);
+                break;
+            case 2:
+                CallEvent(event, values[0], values[1]);
+                break;
+            case 3:
+                CallEvent(event, values[0], values[1], values[2]);
+                break;
+            case 4:
+                CallEvent(event, values[0], values[1], values[2], values[3]);
+                break;
+            case 5:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4]);
+                break;
+            case 6:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4], values[5]);
+                break;
+            case 7:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+                break;
+            default:
+                CallEvent(event, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+                break;
+        }
     }
-    export function callRemote(player: Player, event: string, ...args: any[]): void {
-        CallRemoteEvent(player.getId(), event, args);
+    export function callRemote(player: Player, event: string, ...values: any[]): void {
+        switch(values.length){
+            case 0:
+                CallRemoteEvent(player.getId(), event);
+                break;
+            case 1:
+                CallRemoteEvent(player.getId(), event, values[0]);
+                break;
+            case 2:
+                CallRemoteEvent(player.getId(), event, values[0], values[1]);
+                break;
+            case 3:
+                CallRemoteEvent(player.getId(), event, values[0], values[1], values[2]);
+                break;
+            case 4:
+                CallRemoteEvent(player.getId(), event, values[0], values[1], values[2], values[3]);
+                break;
+            case 5:
+                CallRemoteEvent(player.getId(), event, values[0], values[1], values[2], values[3], values[4]);
+                break;
+            case 6:
+                CallRemoteEvent(player.getId(), event, values[0], values[1], values[2], values[3], values[4], values[5]);
+                break;
+            case 7:
+                CallRemoteEvent(player.getId(), event, values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+                break;
+            default:
+                CallRemoteEvent(player.getId(), event, values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+                break;
+        }
+    }
+    export function addCommand(name: string, handler: Function): void {
+        AddCommand(name, handler);
     }
     export function exit(): void {
         ServerExit();
